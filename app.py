791f4b15fed6f43d1852a781637a0a7dc6b432e5
Fazan456 <mohamedfazan76@gmail.com>
2024-01-07 14:02:49 +0530

    Changes in table_list and util.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import sys
 from config import DB_DETAILS
+from util import get_tables
 
 def main():
     # program takes at leats one argument
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
-    print(db_details)
-
+    tables=get_tables('table_list')
+    for table in tables['table_name']:
+        print(table)
 
 
 
